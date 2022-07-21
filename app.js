7221c47c2a6b2b6d9ce35189d75ddbe4f3a87b84
Colin Edwards <ColinJhEdwards@gmail.com>
2022-07-20 19:48:19 -0700

    notes on unshift method for linked list

--- app.js
+++ app.js
@@ -383,10 +383,24 @@ class SinglyLinkedList {
   }
   shift() {
     if (!this.head) return undefined;
-    currentHead = this.head;
+    let currentHead = this.head;
     this.head = currentHead.next;
     this.length--;
+    if (this.length === 0) {
+      this.tail === null;
+    }
     return currentHead;
+  }
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head === newNode;
+      this.tail === this.head;
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
   }
 }
 
