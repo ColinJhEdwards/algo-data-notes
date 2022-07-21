79d95028aff2e4a5c78075fe1f276fa1857d09cd
Colin Edwards <ColinJhEdwards@gmail.com>
2022-07-20 19:23:31 -0700

    notes on linked list methods

--- app.js
+++ app.js
@@ -364,6 +364,30 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head === null;
+      this.tail === null;
+    }
+    return current;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    return currentHead;
+  }
 }
 
 list = new SinglyLinkedList();
