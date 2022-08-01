adb0b87d42e4fdde61594d4dc75ad0a2d765b5ee
Colin Edwards <ColinJhEdwards@gmail.com>
2022-07-31 19:31:47 -0700

    starting notes on double linked lists

--- app.js
+++ app.js
@@ -483,100 +483,32 @@ class LinkedList {
     const newNode = new Node(val);
     if (!this.head) {
       this.head = newNode;
-      this.tail = newNode;
+      this.tail = this.head;
     } else {
       this.tail.next = newNode;
       this.tail = newNode;
     }
-    this.length++;
+    length++;
     return this;
   }
   pop() {
-    if (this.length === 0) {
-      return undefined;
-    }
+    if (!this.head) return null;
     let current = this.head;
-    let newTail = current;
-    while (current.next) {
-      newTail = current;
+    let prev = current;
+    while (current) {
+      prev = current;
       current = current.next;
     }
-    this.tail = newTail;
+    this.tail = prev;
     this.tail.next = null;
     this.length--;
-    if (this.length === 0) {
-      this.head = null;
-      this.tail = null;
-    }
     return current;
   }
-  shift() {
-    if (!this.head) return undefined;
-    const originalHead = this.head;
-    this.head = originalHead.next;
-    this.length--;
-    return originalHead;
-  }
-  unshift(val) {
-    const newNode = new Node(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-    this.length++;
-    return this;
-  }
-  get(index) {
-    if (index < 0 || index >= this.length) return null;
-    let current = this.head;
-    for (let i = 0; i < index.length; i++) {
-      current = current.next;
-    }
-    return current;
-  }
-  set(index, val) {
-    const node = this.get(index);
-    if (node) {
-      node.value = val;
-      return true;
-    }
-    return false;
-  }
-  insert(index, val) {
-    if (index < 0 || index > this.length) return null;
-    if (index === this.length) {
-      this.push(val);
-      return true;
-    }
-    if (index === 0) {
-      this.unshift(val);
-      return true;
-    }
-    const prevNode = this.get(index - 1);
-    const nextNode = prevNode.next;
-    const newNode = new Node(val);
-    prevNode.next = newNode;
-    newNode.next = nextNode;
-    this.length++;
-    return true;
-  }
-  remove(index) {
-    if (index < 0 || index >= this.length) return null;
-    if (index === this.length - 1) {
-      this.pop();
-      return true;
-    }
-    if (index === 0) {
-      this.shift(val);
-      return true;
-    }
-    const prev = this.get(index - 1);
-    const deleted = prev.next;
-    prev.next = deleted.next;
-    this.length--;
-    return deleted;
-  }
+  shift() {}
+  unshift(val) {}
+  get(index) {}
+  set(index, val) {}
+  insert(index, val) {}
+  remove(index) {}
+  reverse() {}
 }
