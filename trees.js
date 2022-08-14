2c00daf025fda1f0cae1d0da201f7a9f56662046
Colin Edwards <ColinJhEdwards@gmail.com>
2022-08-14 10:18:23 -0700

    practice with find method for binary search tree

--- trees.js
+++ trees.js
@@ -18,15 +18,32 @@ class BinarySearchTree {
     } else {
       let current = this.root;
       while (true) {
+        if (value === current.value) return undefined;
         if (val < current.value) {
           if (current.left === null) {
             current.left = newNode;
             return this;
-          } else {
+          } else if (value > current.value) {
             current = current.left;
           }
         }
       }
     }
+  }
+  find(val) {
+    if (!this.root) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (val < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return false;
+    return current;
   }
 }
