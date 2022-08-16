ae1ef57ebb44a9c769370c7c544ce2bfa4e1a21c
Colin Edwards <ColinJhEdwards@gmail.com>
2022-08-16 07:50:30 -0700

    practice with insert method for max binary heap

--- heaps.js
+++ heaps.js
@@ -2,4 +2,23 @@ class MaxBinaryHeap {
   constructor() {
     this.values = [];
   }
+  insert(val) {
+    this.values.push(val);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let index = this.values.length - 1;
+    const element = this.values[index];
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
+      if (element > parent) {
+        this.values[parentIndex] = element;
+        this.values[index] = parent;
+        index = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
 }
