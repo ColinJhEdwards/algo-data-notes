d82150464d9e5ae6948492b0bdd1b586e41c2973
Colin Edwards <ColinJhEdwards@gmail.com>
2022-08-22 08:01:36 -0700

    notes on priority queues

--- heaps.js
+++ heaps.js
@@ -62,3 +62,74 @@ class MaxBinaryHeap {
     }
   }
 }
+
+class Node {
+  constructor(val, priority) {
+    this.value = val;
+    this.priority = priority;
+  }
+}
+
+// Note that this queue is implemented using a max binary heap, this could also be done with a min binary heap. Doesnt really matter which one was long as you're consistant
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  insert(val, priority) {
+    const newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let index = this.values.length - 1;
+    const element = this.values[index];
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
+      if (element.priority <= parent.priority) break;
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
+    }
+  }
+  extractMax() {
+    const oldRoot = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+
+    return oldRoot;
+  }
+  sinkDown() {
+    let index = 0;
+    const length = this.values.length;
+    while (index < length) {
+      let leftChildIndex = index * 2 + 1;
+      let rightChildIndex = index * 2 + 2;
+      let leftChild;
+      let rightChild;
+      let swap = null;
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild.priority > this.values[0].priority) {
+          swap = leftChildIndex;
+        }
+      }
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild.priority > this.values[0].priority) ||
+          (swap !== null && rightChild.priority > leftChild.priority)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+      if (swap === null) break;
+      this.values[index] = this.values[swap];
+      this.vlaues[swap] = this.values[0];
+      index = swap;
+    }
+  }
+}
