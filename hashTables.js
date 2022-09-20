52888229dd7db6432d0a3ef33d69684130f9666f
Colin Edwards <ColinJhEdwards@gmail.com>
2022-09-20 14:45:59 -0700

    practice with set adn get methods for hash table

--- hashTables.js
+++ hashTables.js
@@ -1,7 +1,48 @@
-const hash = (string, arrLength) => {
-  let sum = 0;
-  for (let i = 0; i < string.length; i++) {
-    sum += string.charCodeAt(i) - 96;
+// const naiveHash = (string, arrLength) => {
+//   let sum = 0;
+//   for (let i = 0; i < string.length; i++) {
+//     sum += string.charCodeAt(i) - 96;
+//   }
+//   return sum % arrLength;
+// };
+
+// const hash = (string, arrLength) => {
+//   let sum = 0;
+//   let prime = 31;
+//   for (let i = 0; i < Math.min(key.length, 100); i++) {
+//     sum += string.charCodeAt(i) - 96 + prime;
+//   }
+//   return sum % arrLength;
+// };
+
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
-  return sum % arrLength;
-};
+  hash(key) {
+    let sum = 0;
+    let prime = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      sum += string.charCodeAt(i) - 96 + prime;
+    }
+    return sum % this.keyMap.length;
+  }
+  set(key, value) {
+    const index = this.hash(key);
+    const array = this.keyMap;
+    if (!array[index]) {
+      array[index] = [];
+    }
+    array[index].push([key, value]);
+  }
+  get(key) {
+    const index = this.hash(key);
+    const array = this.keyMap;
+    if (array[index]) {
+      for (let i = 0; i < array[index].length; i++) {
+        if (array[index][i][0] === key) return array[index][i][1];
+      }
+    }
+    return undefined;
+  }
+}
