87e6a8b3bccfe604e1560288072c98ae61f0fafc
Colin Edwards <ColinJhEdwards@gmail.com>
2022-10-03 21:16:03 -0700

    created priority queue class for algo

--- dijkstras.js
+++ dijkstras.js
@@ -10,3 +10,19 @@ class WeightedGraph {
     this.adjacencyList[v2].push({ node: v1, weight });
   }
 }
+
+class ProQue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, prio) {
+    this.values.push({ val, prio });
+    this.sort();
+  }
+  dequeue() {
+    return this.values.shift();
+  }
+  sort() {
+    this.values.sort((a, b) => a.prio - b.prio);
+  }
+}
